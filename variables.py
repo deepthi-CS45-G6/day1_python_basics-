711c6e391d739f5ef5adb7e1e79ea522f5ed13b3
deepthi-CS45-G6 <saideepthiCS45-G6@coastalseven.com>
2026-05-18 12:21:46 +0530

    Add files via upload
    Variable are used to store data in the memory.
    Rules for creating variable name:
    1. Cannot start with a number.
    2. Special characters are not allowed except underscore
    3. No spaces. 
    variable names are case-sensitive in Python.

---
+++ variables.py
@@ -0,0 +1,35 @@
+student_name="Sai Deepthi"
+student_age=20
+student_college="VVIT"
+student_city="Guntur"
+print("Student name:", student_name)
+print("Student age:", student_age)
+print("Student college:", student_college)
+print("Student city:", student_city)
+#Variable Updating
+student_college="VVITU"
+print("Updated student college:")
+#Multiple Assignment
+x,y,z=10,20,30
+print("value of x:", x)
+print("value of y:", y)
+print("value of z", z)
+#same Assignment
+a=b=c=100
+print("value of a,b,c:",  a,b,c)
+#String concatenation
+first_name="Sai"
+last_name="Deepthi"
+full_name=first_name+last_name
+print("Full Name:", full_name)
+#Swapping variables
+num1, num2= 10, 50
+print("Before Swapping::", num1, num2)
+num1, num2= num2, num1
+print("After Swapping:", num1, num2)
+#User input
+user_name=input("Enter your name: ")
+user_age=int(input("enter your age: "))
+print("User Name:", user_name)
+print("User Age:", user_age)
+
